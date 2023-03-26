a070b67f9e54a9f191652bee3be683d346385b43
Artem S <frees.comp@gmail.com>
2023-03-26 12:25:11 +0300

    some fix

--- server.js
+++ server.js
@@ -3,13 +3,10 @@ const path = require("path");
 
 const app = express();
 
-// Добавить middleware для обслуживания статических файлов из папки 'build'
 app.use(express.static(path.join(__dirname, "build")));
 
-// Добавить обработчик для всех GET-запросов, которые не соответствуют другим маршрутам
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-// Запустить сервер на порту 5000
 app.listen(5000);
